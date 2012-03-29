e9d04106541ecbba9cad67a1dc14d9d8ce7ef99d
Christophe <Popfr1@gmail.com>
2012-03-29 21:20:25 +0200

    ts

--- src/my/pack/age/TestActivity.java
+++ src/my/pack/age/TestActivity.java
@@ -9,6 +9,5 @@ public class TestActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        ///BOummmm
     }
 }
