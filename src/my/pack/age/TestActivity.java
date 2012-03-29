037a36328b079fa6d9323fafbbbe3c841142f3b3
Christophe <Popfr1@gmail.com>
2012-03-29 20:35:51 +0200

    HIiiii

---
+++ src/my/pack/age/TestActivity.java
@@ -0,0 +1,14 @@
+package my.pack.age;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ///BOummmm
+    }
+}
